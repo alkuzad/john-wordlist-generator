50026ac35341ba4586cb2295e2527a34910e84f4
Dawid Gosławski <dawid@dawidgoslawski.pl>
2020-12-29 14:45:45 +0100

    Create wordlist.py

---
+++ wordlist.py
@@ -0,0 +1,31 @@
+from itertools import product
+
+# Fill with common prefixes, starting both with upcase and downcase
+prefixes = {}
+
+# Fill with all common prefixes like !
+suffixes = {}
+
+pro_pre_suf = []
+
+for repeat in range(1, 5):
+    print(f"Repeat: {repeat}")
+    pro_mix = {''.join(x) for x in product(prefixes, repeat=repeat)}
+    for suf_repeat in range(1,5):
+        print(f"Suf Repeat: {suf_repeat}")
+        suff_mix = {''.join(x) for x in product(suffixes, repeat=suf_repeat)}
+        for pro in pro_mix:
+            for suf in suff_mix:
+                pro_pre_suf.append(f"{pro}{suf}")
+                if(len(pro_pre_suf) == 300000):
+                    with open('wordlist-generated.txt', 'a') as wordlist:
+                        stro = "\n".join(set(pro_pre_suf))
+                        wordlist.write(stro)
+                        print("Wrote batch")
+                    pro_pre_suf = []
+
+if pro_pre_suf:
+    with open('wordlist-generated.txt', 'a') as wordlist:
+        stro = "\n".join(set(pro_pre_suf))
+        wordlist.write(stro)
+        print("Wrote remaining batch")
